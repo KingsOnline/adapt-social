1d7e4721e6f0d286196b09f47072be1287a61feb
Simon  Date <simondate@yahoo.co.uk>
2017-06-30 17:32:29 +0100

    Improved function for how blocks with attribute are found. Using HBS to create iframe.

--- js/adapt-social.js
+++ js/adapt-social.js
@@ -1,40 +1,31 @@
 define(function(require) {
 
   var Adapt = require('coreJS/adapt');
-  var Backbone = require('backbone');
   var socialView = require('extensions/adapt-social/js/adapt-socialView');
 
-
   Adapt.once('sideView:loaded', function() {
-    createSocial();
+    createIframe();
   });
 
   Adapt.on('sideView:pageReady', function() {
-      console.log(Adapt.location);
-      loopBlocksInPage(Adapt.contentObjects._byAdaptID[Adapt.location._currentId]);
+      setupSocial(Adapt.contentObjects._byAdaptID[Adapt.location._currentId][0]);
   });
 
-  function loopBlocksInPage(currentPage) {
-    var articles = currentPage[0].attributes._children;
-    for (var i = 0; i < articles.length; i++) { // loop through all the articles in the page
-      var blocks = articles.models[i].attributes._children;
-      for (var v = 0; v < blocks.models.length; v++) {
-        var block = blocks.models[v];
-        checkForMoodleAttr(block);
+  function setupSocial(currentPage) {
+    this._socialBlocks = currentPage.findDescendants('blocks').filter(function(model) {
+      return model.get("_social");
+    });
+    _.each(this._socialBlocks, function(blockModel) {
+      if (typeof blockModel.attributes._social != "undefined") {
+        if (blockModel.attributes._social._isEnabled != false) {
+          new socialView(blockModel);
+        }
       }
-    }
+    });
   }
 
-  function checkForMoodleAttr(blockModel) {
-    var context = this;
-    if (typeof blockModel.attributes._social != "undefined") {
-      if (blockModel.attributes._social._isEnabled != false) {
-        new socialView(blockModel);
-      }
-    }
-  }
-
-  function createSocial() {
-    $('.sideview-iframe-holder').append("<div class='social hidden'><iframe name='socialIframe' id='social-iframe' src='' class='social-iframe'></iframe></div>");
+  function createIframe() {
+    var template = Handlebars.templates.socialIframe;
+    $('.sideview-iframe-holder').append(template());
   }
 });
